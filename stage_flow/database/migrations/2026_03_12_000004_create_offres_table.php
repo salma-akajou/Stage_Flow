35e98a00db022e077d468c081bf32e3a839e0171
salma akg <salmaakagou1@gmail.com>
2026-04-10 09:28:25 +0100

    feat : added services

--- stage_flow/database/migrations/2026_03_12_000004_create_offres_table.php
+++ stage_flow/database/migrations/2026_03_12_000004_create_offres_table.php
@@ -8,6 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
+        
         Schema::create('offres', function (Blueprint $table) {
             $table->id();
             $table->string('titre');
